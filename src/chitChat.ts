06ce1d21e5fd362fbaee3631f49f0dee1681f484
Elliot Voris <elliot@stellar.org>
2025-05-01 10:03:02 -0500

    make the message cards less string-based

--- src/chitChat.ts
+++ src/chitChat.ts
@@ -1,8 +1,8 @@
-import { scrollToBottom, truncate } from "./utils";
+import { scrollToBottom } from "./utils";
 import { account, server } from "./passkeys";
 import { Client as SnapchainClient, networks, type ChatMessage } from "snapchain_sdk";
 import { Api } from "@stellar/stellar-sdk/rpc";
-import { createChatLedgerKeys, getNextIndex, rpc } from "./stellar";
+import { createChatLedgerKeys, getNextIndex, rpc, stellarExpertLink } from "./stellar";
 import { scValToNative } from "@stellar/stellar-sdk";
 
 const snapchain = new SnapchainClient({
@@ -19,22 +19,20 @@ export class ChatLog {
     constructor(element: HTMLDivElement) {
         this.element = element
         this.refreshChatHistory()
-            .then(() => scrollToBottom())
+            .then(scrollToBottom)
     }
 
     renderMessages() {
-        let placeholder = ''
-        Object.entries(this.messages)
-            .forEach(([_, chatMessage]) => {
-                placeholder += renderMessage(chatMessage)
-            })
-        this.element.innerHTML = placeholder
+        let messageElements = Object.entries(this.messages)
+            .map(([_, chatMessage]) => renderMessage(chatMessage))
+        this.element.replaceChildren(...messageElements)
     }
 
     async sendMessage(message: string, author: string, keyId: string) {
         if (!account.wallet?.options) {
             account.connectWallet({ keyId, })
         }
+
         let at = await snapchain.send({
             author,
             message,
@@ -83,14 +81,30 @@ export class ChatLog {
     }
 }
 
-function renderMessage({author, message, timestamp}: ChatMessage): string {
-    return `
-        <article class="chat-card">
-            <header><nav>
-                <small>${truncate(author)}</small>
-                <small>${new Date(Number(timestamp) * 1_000).toLocaleString()}</small>
-            </nav></header>
-            <p>${message}</p>
-        </article>
-    `
+function renderMessage({author, message, timestamp}: ChatMessage): HTMLElement {
+    // create the main chunks of a message card
+    const article = document.createElement('article')
+    article.classList.add('chat-card')
+    const header = document.createElement('header')
+    const nav = document.createElement('nav')
+
+    // fill in the "header" of the message card
+    const smallAuthor = document.createElement('small')
+    const authorLink = stellarExpertLink(author)
+    smallAuthor.append(authorLink)
+    const smallTimestamp = document.createElement('small')
+    smallTimestamp.textContent = new Date(Number(timestamp) * 1_000).toLocaleString()
+
+    // create he message paragraph element
+    const messageP = document.createElement('p')
+    messageP.textContent = message
+
+    // assemble the card
+    nav.append(smallAuthor, smallTimestamp)
+    header.append(nav)
+    article.append(header)
+    article.append(messageP)
+
+    // return the constructed message card
+    return article
 }
